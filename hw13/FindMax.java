eb5afe0919dea176dafe0ef14a2eaaa2ef6f10d0
Yevhenii Donenko <shooamf@gmail.com>
2019-12-12 18:55:07 +0200

    hw13

--- hw13/FindMax.java
+++ hw13/FindMax.java
@@ -11,11 +11,10 @@ public class FindMax {
 		long delta = System.currentTimeMillis() - current;
 		System.out.println("Без многопоточности нашел максимум (" + max + ") за " + delta + " мс");
 
-//		long current2 = System.currentTimeMillis();
-//		int max2 = new FindMaxByMulti().maxByMylti(matrix);
-//		long delta2 = System.currentTimeMillis() - current2;
-//		System.out.println("А с разделением потоков " + max2 +  " нашел за " + delta2 + " мс");
-		System.out.println("А с разделением потоков так и не нашел...");
+		long current2 = System.currentTimeMillis();
+		int max2 = new FindMaxByMulti().maxByMulti(matrix);
+		long delta2 = System.currentTimeMillis() - current2;
+		System.out.println("А с разделением потоков " + max2 +  " нашел за " + delta2 + " мс");
 
 	}
 }
